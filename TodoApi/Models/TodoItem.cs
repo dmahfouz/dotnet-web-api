7a0053b0fd08aac6e2d7b1c053ad2cce67903fe9
dmahfouz <david.mahfouz@gmail.com>
2020-05-05 18:20:35 +0100

    Added data transfer object to code

--- TodoApi/Models/TodoItem.cs
+++ TodoApi/Models/TodoItem.cs
@@ -6,3 +6,9 @@ public class TodoItem
     public string Secret { get; set; }
 }
 
+// public class TodoItemDTO
+// {
+//     public long Id { get; set; }
+//     public string Name { get; set; }
+//     public bool IsComplete {get; set; }
+// }
